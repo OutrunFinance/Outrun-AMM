52a45c3369e27fbecb43eb3d2a2524f5959494a8
Jason <2045330878@qq.com>
2024-02-28 17:50:05 +0800

    [add] add getter

--- src/core/interfaces/IOutswapV1Pair.sol
+++ src/core/interfaces/IOutswapV1Pair.sol
@@ -18,6 +18,8 @@ interface IOutswapV1Pair {
     function factory() external view returns (address);
     function token0() external view returns (address);
     function token1() external view returns (address);
+    function ffPairFeeTo() external view returns (address);
+    function ffPairFeeExpireTime() external view returns (uint);
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
     function price0CumulativeLast() external view returns (uint);
     function price1CumulativeLast() external view returns (uint);
