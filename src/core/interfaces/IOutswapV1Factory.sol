a4f22ea0465d77189d417b98b26d42ab5462aaf7
Jason <2045330878@qq.com>
2024-03-24 23:40:50 +0800

    [opt] use ownable

--- src/core/interfaces/IOutswapV1Factory.sol
+++ src/core/interfaces/IOutswapV1Factory.sol
@@ -2,19 +2,13 @@
 pragma solidity ^0.8.24;
 
 interface IOutswapV1Factory {
-    event PairCreated(
-        address indexed token0, address indexed token1, address pair, uint256
-    );
-
     function feeTo() external view returns (address);
-    function feeToSetter() external view returns (address);
-
-    function getPair(address tokenA, address tokenB) external view returns (address pair);
     function allPairs(uint256) external view returns (address pair);
     function allPairsLength() external view returns (uint256);
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
 
     function createPair(address tokenA, address tokenB) external returns (address pair);
-
     function setFeeTo(address) external;
-    function setFeeToSetter(address) external;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint256);
 }
