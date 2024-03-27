e40c5216dd9958b7fdf884cf69ecbd944c3f11e5
Chocolatieee0929 <luoruixx@163.com>
2024-03-27 20:49:54 +0800

    test: swap token and usdb

--- test/USDBAddLiquidityTest.sol
+++ test/USDBAddLiquidityTest.sol
@@ -5,6 +5,7 @@ pragma abicoder v2;
 import {BaseDeploy} from "./BaseDeploy.t.sol";
 import {OutswapV1Library} from 'src/libraries/OutswapV1Library.sol';
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IOutswapV1Pair} from "src/core/interfaces/IOutswapV1Pair.sol";
 
 contract RouterUSDBMOCK is BaseDeploy {
     uint256 public constant MINIMUM_LIQUIDITY = 1000;
@@ -13,29 +14,104 @@ contract RouterUSDBMOCK is BaseDeploy {
 
     /* using USDB and token0 to add liquidity */
     function test_addLiquidityUSDB() public {
-        uint256 liquidity = addLiquidityTokenAndUSDB(tokens[0]);
+        (,,uint256 liquidity, )  = addLiquidityTokenAndUSDB(tokens[0],1 ether,1 ether);
         assertEq(liquidity, 1 ether - MINIMUM_LIQUIDITY);
     }
 
     function test_addLiquidityETHAndUSDB() public {
-        uint256 liquidity = addLiquidityTokenAndUSDB(RETH9);
+        (,,uint256 liquidity, )  = addLiquidityTokenAndUSDB(RETH9,1 ether,1 ether);
         assertEq(liquidity, 1 ether - MINIMUM_LIQUIDITY);
     }
 
+    function test_AddLiquidityUSDBNoPair() public {
+        (uint256 amount0, uint256 amount1, uint256 liquidity, address pair)  = addLiquidityTokenAndUSDB(tokens[0],1 ether,1 ether);
+
+        assertEq(amount0, 1 ether);
+        assertEq(amount1, 1 ether);
+        assertEq(liquidity, 1 ether - MINIMUM_LIQUIDITY);
+
+        assertEq(IOutswapV1Pair(pair).token0(), tokens[0]);
+        assertEq(IOutswapV1Pair(pair).token1(), RUSD9);
+
+        (uint256 reserve0, uint256 reserve1, ) = IOutswapV1Pair(pair)
+            .getReserves();
+        assertEq(reserve0, 1 ether);
+        assertEq(reserve1, 1 ether);
+        assertEq(IERC20(RUSD9).balanceOf(address(pair)), 1 ether);
+        assertEq(IERC20(tokens[0]).balanceOf(address(pair)), 1 ether);
+    }
+
+    function test_AddLiquidityInsufficientUsdbAmount() public {
+        (,,uint256 liquidity, )  = addLiquidityTokenAndUSDB(tokens[0], 4 ether, 8 ether);
+
+        vm.startPrank(deployer);
+        IERC20(tokens[0]).approve(address(swapRouter), 1 ether);
+        IERC20(USDB).approve(address(swapRouter), 2 ether);
+
+        vm.expectRevert(bytes("OutswapV1Router: INSUFFICIENT_B_AMOUNT"));
+        swapRouter.addLiquidityUSDB(
+            tokens[0],
+            1 ether,
+            2 ether,
+            1 ether,
+            2.3 ether,
+            address(this),
+            block.timestamp + 1
+        );
+        vm.stopPrank();
+    }
+
+    function testAddLiquidityExpired() public {
+        vm.startPrank(deployer);
+        rusd.approve(address(swapRouter), 1 ether);
+        IERC20(tokens[0]).approve(address(swapRouter), 1 ether);
+
+        vm.warp(2);
+        vm.expectRevert(bytes("OutswapV1Router: EXPIRED"));
+        swapRouter.addLiquidityUSDB(
+            tokens[0],
+            1 ether,
+            1 ether,
+            1 ether,
+            1 ether,
+            address(this),
+            1
+        );
+        vm.stopPrank();
+    }
+
+    /* remove liquidity */
     function test_removeLiquidityUSDB() public {
-        uint256 liquidity = addLiquidityTokenAndUSDB(tokens[0]);
+        (,,uint256 liquidity, ) = addLiquidityTokenAndUSDB(tokens[0],1 ether,1 ether);
         removeLiquidityTokenAndUSDB(tokens[0], liquidity);
     }
 
     function test_removeLiquidityETHAndUSDB() public {
-        uint256 liquidity = addLiquidityTokenAndUSDB(RETH9);
+        (,,uint256 liquidity, ) = addLiquidityTokenAndUSDB(RETH9,1 ether,1 ether);
         removeLiquidityTokenAndUSDB(RETH9, liquidity);
     }
 
-    function addLiquidityTokenAndUSDB(address token) internal returns (uint256) {
-        uint256 liquidity;
+    /* swap token and usdb */
+    function test_swapExactUSDBForTokens() public {
+        (,,uint256 liquidity, )  = addLiquidityTokenAndUSDB(tokens[0],1 ether,1 ether);
+
+        address[] memory path = new address[](2);
+        path[0] = RUSD9;
+        path[1] = tokens[0];
+
+        vm.startPrank(deployer);
+        IERC20(USDB).approve(address(swapRouter), 5000);
+        uint256[] memory amounts = swapRouter.swapExactUSDBForTokens(5000, 4000, path, address(this), block.timestamp + 100);
+        vm.stopPrank();
+
+        uint256[] memory amountsCal = OutswapV1Library.getAmountsOut(OutswapV1Factory, 5000, path);
+        assertEq(amounts[0], amountsCal[0]);
+    }
+
+    /* function */
+    function addLiquidityTokenAndUSDB(address token, uint256 tokenAmount, uint256 usdbAmount) internal returns (uint256 amount0, uint256 amount1, uint256 liquidity, address pair) {
         (address _token0, address _token1) = OutswapV1Library.sortTokens(token, RUSD9);
-        address pair = OutswapV1Library.pairFor(
+        pair = OutswapV1Library.pairFor(
             address(poolFactory),
             _token0,
             _token1
@@ -43,24 +119,24 @@ contract RouterUSDBMOCK is BaseDeploy {
 
         vm.startPrank(deployer);
 
-        IERC20(USDB).approve(address(swapRouter), 1 ether);
+        IERC20(USDB).approve(address(swapRouter), usdbAmount);
         if(token != RETH9) {
-            IERC20(tokens[0]).approve(address(swapRouter), 1 ether);
-            (, , liquidity) = swapRouter.addLiquidityUSDB{value: 1 ether}(
+            IERC20(tokens[0]).approve(address(swapRouter), tokenAmount);
+            (amount0, amount1, liquidity) = swapRouter.addLiquidityUSDB(
                 tokens[0],
-                1 ether,
-                1 ether,
-                1 ether,
-                1 ether,
+                tokenAmount,
+                usdbAmount,
+                tokenAmount,
+                usdbAmount,
                 address(this),
                 block.timestamp + 1
             );
         }
         else {
-            (, , liquidity) = swapRouter.addLiquidityETHAndUSDB{value: 1 ether}(
-                1 ether,
-                1 ether,
-                1 ether,
+            (amount0, amount1, liquidity) = swapRouter.addLiquidityETHAndUSDB{value: tokenAmount}(
+                usdbAmount,
+                tokenAmount,
+                usdbAmount,
                 address(this),
                 block.timestamp + 1
             );
@@ -68,8 +144,6 @@ contract RouterUSDBMOCK is BaseDeploy {
 
         assertEq(poolFactory.getPair(_token0, _token1), pair);
         vm.stopPrank();
-
-        return liquidity;
     }
 
     function removeLiquidityTokenAndUSDB(address token, uint256 amount) internal {
@@ -98,24 +172,8 @@ contract RouterUSDBMOCK is BaseDeploy {
         }
 
     }
+
     /* 
-    function removeLiquidityUSDB(
-        address token,
-        uint256 liquidity,
-        uint256 amountTokenMin,
-        uint256 amountUSDBMin,
-        address to,
-        uint256 deadline
-    ) external returns (uint256 amountToken, uint256 amountUSDB);
-
-    function removeLiquidityETHAndUSDB(
-        uint256 liquidity,
-        uint256 amountETHMin,
-        uint256 amountUSDBMin,
-        address to,
-        uint256 deadline
-    ) external returns (uint256 amountETH, uint256 amountUSDB);
-
     function swapExactUSDBForTokens(
         uint256 amountIn,
         uint256 amountOutMin,
