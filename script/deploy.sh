9b495cfba01cef3b0131cb7c692ca51ffdfd8237
Jason <2045330878@qq.com>
2024-06-07 12:50:26 +0800

    update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,8 @@
 source ../.env
-forge clean && forge build
+#forge clean && forge build
 forge script OutswapV1Script.s.sol:OutswapV1Script --rpc-url blast_sepolia --priority-gas-price 300 --with-gas-price 1200000 --broadcast --verify --ffi -vvvv
+
+# forge script OutswapV1Script.s.sol:OutswapV1Script --chain-id 168587773 --rpc-url blast_sepolia \
+#     --etherscan-api-key $BLASTSCAN_API_KEY --verifier-url $BLAST_API_URL \
+#     --priority-gas-price 300 --with-gas-price 1200000 \
+#     --broadcast --verify --ffi -vvvv
