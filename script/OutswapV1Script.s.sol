273a2b5718d389f4b55808b3f731899bd8666593
Jason <2045330878@qq.com>
2024-06-11 04:29:04 +0800

    redeploy

--- script/OutswapV1Script.s.sol
+++ script/OutswapV1Script.s.sol
@@ -8,22 +8,39 @@ import "../src/core/OutswapV1Factory.sol";
 import "../src/router/OutswapV1Router.sol";
 
 contract OutswapV1Script is BaseScript {
+    address internal orETH;
+    address internal orUSD;
+    address internal USDB;
+    address internal owner;
+    address internal feeTo;
+    address internal gasManager;
+
+    OutswapV1Factory internal factory;
+
     function run() public broadcaster {
-        address orETH = vm.envAddress("ORETH");
-        address orUSD = vm.envAddress("ORUSD");
-        address USDB = vm.envAddress("USDB");
-        address owner = vm.envAddress("OWNER");
-        address feeTo = vm.envAddress("FEE_TO");
-        address gasManager = vm.envAddress("GAS_MANAGER");
+        orETH = vm.envAddress("ORETH");
+        orUSD = vm.envAddress("ORUSD");
+        USDB = vm.envAddress("USDB");
+        owner = vm.envAddress("OWNER");
+        feeTo = vm.envAddress("FEE_TO");
+        gasManager = vm.envAddress("GAS_MANAGER");
         
-        OutswapV1Factory factory = new OutswapV1Factory(owner, gasManager);
-        factory.setFeeTo(feeTo);
-        OutswapV1Router router = new OutswapV1Router(address(factory), orETH, orUSD, USDB, gasManager);
-
-        console.log("OutswapV1Factory deployed on %s", address(factory));
-        console.log("OutswapV1Router deployed on %s", address(router));
-
         bytes32 initCodeHash = keccak256(abi.encodePacked(type(OutswapV1Pair).creationCode, abi.encode(gasManager)));
         console.logBytes32(initCodeHash);
+
+        //deployFactory();
+        deployRouter(0x583758BBD5B5fAF2983Be70B8E551829E1fbCc91);
+    }
+
+    function deployFactory() internal {
+        factory = new OutswapV1Factory(owner, gasManager);
+        factory.setFeeTo(feeTo);
+
+        console.log("OutswapV1Factory deployed on %s", address(factory));
+    }
+
+    function deployRouter(address factoryAddr) internal {
+        OutswapV1Router router = new OutswapV1Router(factoryAddr, orETH, orUSD, USDB, gasManager);
+        console.log("OutswapV1Router deployed on %s", address(router));
     }
 }
--- src/libraries/OutswapV1Library.sol
+++ src/libraries/OutswapV1Library.sol
@@ -23,7 +23,7 @@ library OutswapV1Library {
                             factory,
                             keccak256(abi.encodePacked(token0, token1)),
                             /* bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(OutswapV1Pair).creationCode, abi.encode(gasManager))); */
-                            hex"4d2be187a72430df6aae8e47abf8adaac5ca81da129a3618a7417d8dbfc12a91" // init code hash
+                            hex"b11291cf74ef5b01d3360f4240ee3ae9b64c14297affa289ced01c032c687976" // init code hash
                         )
                     )
                 )
