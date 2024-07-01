3ab3e908d6add9112529f99a08e93a48d8301c48
Jason <2045330878@qq.com>
2024-07-01 20:55:21 +0800

    deploy multicall

--- script/OutswapV1Script.s.sol
+++ script/OutswapV1Script.s.sol
@@ -8,6 +8,7 @@ import "../src/core/OutswapV1Pair1.sol";
 import "../src/core/OutswapV1Factory0.sol";
 import "../src/core/OutswapV1Factory1.sol";
 import "../src/router/OutswapV1Router.sol";
+import "../src/router/OutrunMulticall.sol";
 
 contract OutswapV1Script is BaseScript {
     address internal orETH;
@@ -42,12 +43,15 @@ contract OutswapV1Script is BaseScript {
         // console.log("OutswapV1Router0 deployed on %s", router0Addr);
 
         // The initCode for the OutswapV1Library needs to be modified first.
-        factory1 = new OutswapV1Factory1(owner, gasManager);
-        factory1.setFeeTo(feeTo);
-        address factory1Addr = address(factory1);
-        address router1Addr = deployRouter(factory1Addr);
-        console.log("OutswapV1Factory1 deployed on %s", factory1Addr);
-        console.log("OutswapV1Router1 deployed on %s", router1Addr);
+        // factory1 = new OutswapV1Factory1(owner, gasManager);
+        // factory1.setFeeTo(feeTo);
+        // address factory1Addr = address(factory1);
+        // address router1Addr = deployRouter(factory1Addr);
+        // console.log("OutswapV1Factory1 deployed on %s", factory1Addr);
+        // console.log("OutswapV1Router1 deployed on %s", router1Addr);
+
+        address multicall = address(new OutrunMulticall(gasManager));
+        console.log("OutrunMulticall deployed on %s", multicall);
 
     }
 
