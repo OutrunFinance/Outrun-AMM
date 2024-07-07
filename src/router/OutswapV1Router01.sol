635b31c0fda5bab9b3dcde48cbf0472cf069eec6
Jason <2045330878@qq.com>
2024-07-07 14:52:58 +0800

    fix multicall referrer error

--- src/router/OutswapV1Router01.sol
+++ src/router/OutswapV1Router01.sol
@@ -356,14 +356,13 @@ contract OutswapV1Router01 is IOutswapV1Router, GasManagerable {
      */
     // requires the initial amount to have already been sent to the first pair
     function _swap(uint256[] memory amounts, address[] memory path, address _to) internal virtual {
+        address referrer = IReferralManager(referralManager).queryReferrer(_to);
         for (uint256 i; i < path.length - 1; i++) {
             (address input, address output) = (path[i], path[i + 1]);
             (address token0,) = OutswapV1Library01.sortTokens(input, output);
             uint256 amountOut = amounts[i + 1];
-            (uint256 amount0Out, uint256 amount1Out) =
-                input == token0 ? (uint256(0), amountOut) : (amountOut, uint256(0));
+            (uint256 amount0Out, uint256 amount1Out) = input == token0 ? (uint256(0), amountOut) : (amountOut, uint256(0));
             address to = i < path.length - 2 ? OutswapV1Library01.pairFor(factory, output, path[i + 2]) : _to;
-            address referrer = IReferralManager(referralManager).queryReferrer(msg.sender);
             IOutswapV1Pair(OutswapV1Library01.pairFor(factory, input, output)).swap(
                 amount0Out, amount1Out, to, referrer, new bytes(0)
             );
