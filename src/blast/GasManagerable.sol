7d6562e0fab9b215d0756fce02a8e906fd0e761b
Jason <2045330878@qq.com>
2024-07-10 10:59:58 +0800

    add ZeroAddress error

--- src/blast/GasManagerable.sol
+++ src/blast/GasManagerable.sol
@@ -8,9 +8,9 @@ abstract contract GasManagerable {
 
     address private _gasManager;
 
-    error UnauthorizedAccount(address account);
+    error ZeroAddress();
 
-    error InvalidGasManager(address gasManager);
+    error UnauthorizedAccount(address account);
 
     event ClaimMaxGas(address indexed recipient, uint256 gasAmount);
 
@@ -18,7 +18,7 @@ abstract contract GasManagerable {
 
     constructor(address initialGasManager) {
         if (initialGasManager == address(0)) {
-            revert InvalidGasManager(address(0));
+            revert ZeroAddress();
         }
         _transferGasManager(initialGasManager);
 
@@ -50,13 +50,17 @@ abstract contract GasManagerable {
      * @param recipient - Address of receive gas
      */
     function claimMaxGas(address recipient) external onlyGasManager returns (uint256 gasAmount) {
+        if (recipient == address(0)) {
+            revert ZeroAddress();
+        }
+
         gasAmount = BLAST.claimMaxGas(address(this), recipient);
         emit ClaimMaxGas(recipient, gasAmount);
     }
 
     function transferGasManager(address newGasManager) public onlyGasManager {
         if (newGasManager == address(0)) {
-            revert InvalidGasManager(address(0));
+            revert ZeroAddress();
         }
         _transferGasManager(newGasManager);
     }
