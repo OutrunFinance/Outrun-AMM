7e375e5cf9b3e8e68db634f01570ebbcca97d97f
Jason <2045330878@qq.com>
2024-07-16 01:02:59 +0800

    custom error for require

--- src/libraries/OutswapV1Library01.sol
+++ src/libraries/OutswapV1Library01.sol
@@ -9,7 +9,17 @@ import "../core/interfaces/IOutswapV1Pair.sol";
 library OutswapV1Library01 {
     error ZeroAddress();
 
+    error InvalidPath();
+
     error IdenticalAddresses();
+
+    error InsufficientAmount();
+    
+    error InsufficientLiquidity();
+
+    error InsufficientInputAmount();
+
+    error InsufficientOutputAmount();
 
     // returns sorted token addresses, used to handle return values from pairs sorted in this order
     function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
@@ -51,15 +61,15 @@ library OutswapV1Library01 {
 
     // given some amount of an asset and pair reserves, returns an equivalent amount of the other asset
     function quote(uint256 amountA, uint256 reserveA, uint256 reserveB) internal pure returns (uint256 amountB) {
-        require(amountA > 0, "OutswapV1Library: INSUFFICIENT_AMOUNT");
-        require(reserveA > 0 && reserveB > 0, "OutswapV1Library: INSUFFICIENT_LIQUIDITY");
+        require(amountA > 0, InsufficientAmount());
+        require(reserveA > 0 && reserveB > 0, InsufficientLiquidity());
         amountB = amountA * reserveB / reserveA;
     }
 
     // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
     function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut) internal pure returns (uint256 amountOut) {
-        require(amountIn > 0, "OutswapV1Library: INSUFFICIENT_INPUT_AMOUNT");
-        require(reserveIn > 0 && reserveOut > 0, "OutswapV1Library: INSUFFICIENT_LIQUIDITY");
+        require(amountIn > 0, InsufficientInputAmount());
+        require(reserveIn > 0 && reserveOut > 0, InsufficientLiquidity());
         uint256 amountInWithFee = amountIn * 997;
         uint256 numerator = amountInWithFee * reserveOut;
         uint256 denominator = reserveIn * 1000 + amountInWithFee;
@@ -68,8 +78,8 @@ library OutswapV1Library01 {
 
     // given an output amount of an asset and pair reserves, returns a required input amount of the other asset
     function getAmountIn(uint256 amountOut, uint256 reserveIn, uint256 reserveOut) internal pure returns (uint256 amountIn) {
-        require(amountOut > 0, "OutswapV1Library: INSUFFICIENT_OUTPUT_AMOUNT");
-        require(reserveIn > 0 && reserveOut > 0, "OutswapV1Library: INSUFFICIENT_LIQUIDITY");
+        require(amountOut > 0, InsufficientOutputAmount());
+        require(reserveIn > 0 && reserveOut > 0, InsufficientLiquidity());
         uint256 numerator = reserveIn * amountOut * 1000;
         uint256 denominator = (reserveOut - amountOut) * 997;
         amountIn = (numerator / denominator) + 1;
@@ -77,7 +87,7 @@ library OutswapV1Library01 {
 
     // performs chained getAmountOut calculations on any number of pairs
     function getAmountsOut(address factory, uint256 amountIn, address[] memory path) internal view returns (uint256[] memory amounts) {
-        require(path.length >= 2, "OutswapV1Library: INVALID_PATH");
+        require(path.length >= 2, InvalidPath());
         amounts = new uint256[](path.length);
         amounts[0] = amountIn;
         for (uint256 i; i < path.length - 1; i++) {
@@ -88,7 +98,7 @@ library OutswapV1Library01 {
 
     // performs chained getAmountIn calculations on any number of pairs
     function getAmountsIn(address factory, uint256 amountOut, address[] memory path) internal view returns (uint256[] memory amounts) {
-        require(path.length >= 2, "OutswapV1Library: INVALID_PATH");
+        require(path.length >= 2, InvalidPath());
         amounts = new uint256[](path.length);
         amounts[amounts.length - 1] = amountOut;
         for (uint256 i = path.length - 1; i > 0; i--) {
