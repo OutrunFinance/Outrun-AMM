67fabcfef2b9cb29545473a6fa9fbf97a843c447
Jason <2045330878@qq.com>
2024-12-18 22:50:23 +0800

    add mantle network

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,17 +1,24 @@
 source ../.env
-# forge clean
-# forge build
+forge clean
+forge build
 
-# forge script OutrunAMMScript.s.sol:OutrunAMMScript --rpc-url bsc_testnet \
-#     --with-gas-price 3000000000 \
+forge script OutrunAMMScript.s.sol:OutrunAMMScript --rpc-url bsc_testnet \
+    --with-gas-price 3000000000 \
+    --optimize --optimizer-runs 100000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify 
+
+# forge script OutrunAMMScript.s.sol:OutrunAMMScript --rpc-url base_sepolia \
+#     --with-gas-price 1200000 \
 #     --optimize --optimizer-runs 100000 \
 #     --via-ir \
 #     --broadcast --ffi -vvvv \
 #     --verify 
 
-forge script OutrunAMMScript.s.sol:OutrunAMMScript --rpc-url base_sepolia \
-    --with-gas-price 1200000 \
-    --optimize --optimizer-runs 100000 \
-    --via-ir \
-    --broadcast --ffi -vvvv \
-    --verify 
+# forge script OutrunAMMScript.s.sol:OutrunAMMScript --rpc-url mantle_sepolia \
+#     --with-gas-price 20000000 \
+#     --optimize --optimizer-runs 100000 \
+#     --via-ir \
+#     --broadcast --ffi -vvvv \
+#     --verify 
